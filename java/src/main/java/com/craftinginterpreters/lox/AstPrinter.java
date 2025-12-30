d64989d670f5af912d40d858fc048f89fb6bd72c
Isaac Leong <isaacleong23367@gmail.com>
2025-12-30 23:52:58 +0800

    Finished Chapter 9

--- java/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ java/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -4,6 +4,7 @@ import com.craftinginterpreters.lox.Expr.Assign;
 import com.craftinginterpreters.lox.Expr.Binary;
 import com.craftinginterpreters.lox.Expr.Grouping;
 import com.craftinginterpreters.lox.Expr.Literal;
+import com.craftinginterpreters.lox.Expr.Logical;
 import com.craftinginterpreters.lox.Expr.Unary;
 import com.craftinginterpreters.lox.Expr.Variable;
 
@@ -42,6 +43,11 @@ class AstPrinter implements Expr.Visitor<String> {
   @Override
   public String visitVariableExpr(Variable expr) {
     return "Getting value for " + expr.name;
+  }
+
+  @Override
+  public String visitLogicalExpr(Logical expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
   }
 
   private String parenthesize(String name, Expr... exprs) {
